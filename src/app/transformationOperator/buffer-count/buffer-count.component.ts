db9fd64ca5902ee15ee647308d6bfcfa746ea924
Priyobrato.Das <priyodas12@gmail.com>
2023-12-27 19:33:25 +0530

    feature: bufferCount example

--- src/app/transformationOperator/buffer-count/buffer-count.component.ts
+++ src/app/transformationOperator/buffer-count/buffer-count.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
+import { bufferCount, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-buffer-count',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./buffer-count.component.css']
 })
 export class BufferCountComponent {
+
+  displayBufferedValues!: number[];
+
+  ngAfterViewInit() {
+    // Create an observable that emits numbers every 500 milliseconds
+    const sourceObservable = interval(500);//.pipe(take(10)); // Emit 10 numbers for the example
+
+    // Use bufferCount to collect numbers into arrays of size 3
+    const bufferedObservable$ = sourceObservable.pipe(bufferCount(3));
+
+    // Subscribe to the buffered observable to receive the arrays
+    const subscription = bufferedObservable$.subscribe(
+      (bufferedValues) => {
+        this.displayBufferedValues = bufferedValues;
+        console.log('Buffered values:', bufferedValues);
+      },
+      (error) => {
+        console.error('Error:', error);
+      },
+      () => {
+        console.log('Complete');
+      }
+    );
+  }
 
 }
