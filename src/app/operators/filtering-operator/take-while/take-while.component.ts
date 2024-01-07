3f7229eb460c2d7b00ee705761a3b23feff139b3
Priyobrato.Das <priyodas12@gmail.com>
2024-01-07 23:15:39 +0530

    feature:takeWhile

--- src/app/operators/filtering-operator/take-while/take-while.component.ts
+++ src/app/operators/filtering-operator/take-while/take-while.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable, interval, fromEvent } from 'rxjs';
+import { take, buffer, takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'app-take-while',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./take-while.component.css']
 })
 export class TakeWhileComponent {
+
+  num: number[] = [];
+
+  status: string = '';
+
+  numberObservable$!: Observable<number>;
+
+  eventObservable$!: Observable<number[]>;
+
+  ngAfterViewInit() {
+    const numbers = interval(100);
+    this.numberObservable$ = numbers;
+
+    const btn = document.getElementById('takeBtn');
+
+    const clickEventObservable$ = fromEvent(btn!, 'click');
+
+    this.eventObservable$ = this.numberObservable$.pipe(
+      takeWhile(val => val < 100),
+      buffer(clickEventObservable$)
+    )
+
+    this.eventObservable$.subscribe(
+      data => { this.num = data },
+      err => console.log(err),
+      () => {
+        this.status = "Completed"
+        console.log('Completed')
+      }
+    )
+  }
 
 }
